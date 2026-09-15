e9e30ef74a950f3aa537f2d2c7946d2cec4e5e61
noobien1 <boemi.pangkey09@gmail.com>
2026-09-15 13:44:35 +0700

    fitur read table anime sudah selesai

---
+++ aniclass-streaming/controllers/catalogController.js
@@ -0,0 +1,18 @@
+const db = require('../config/db');
+
+exports.index = async (req, res, next) => {
+  try {
+    const sql = `select id,title, slug, synopsis, genre, year,poster_url,status
+    from anime
+    order by create_at desc, id desc`;
+
+    const [animeList] = await db.query(sql);
+
+    res.render('catalog/index', {
+      title: 'Daftar Anime',
+      animeList,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
